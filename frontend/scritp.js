d9cc7379073a1c61c7161c6a5682417649fe208d
D-Nyx <jimneznelias@gmail.com>
2025-08-07 02:35:09 -0500

    se cambio el diseño y agrego un mensaje de error

--- frontend/scritp.js
+++ frontend/scritp.js
@@ -3,21 +3,36 @@ const input = document.querySelector('#input')
 const button = document.querySelector('#button')
 const error = document.querySelector(".error")
 const errorLink = document.querySelector(".errorLink")
+button.disabled = true
+//Buton evento hover
+
+input.addEventListener('click', () => {
+    button.classList.remove("ButtonDesactivado")
+    button.disabled = false
+    button.classList.remove("ButtonMensaje")
 
 
 
+})
+
+input.addEventListener('blur', () => button.classList.add("ButtonDesactivado"))
 button.addEventListener('click', () => {
-    const info = input.value
-    const link = info.startsWith("https://youtu.be/")
-    if (info === "") {
-        errorLink.classList.remove("error2")
-        error.classList.add("error2")
+    const info = input.value.trim()
+
+    if (!info.startsWith("https://youtu.be/") && !info.startsWith("https://www.youtube.com/watch?v=")) {
+        errorLink.classList.add("error2")
+        button.classList.add("ButtonDesactivado")
+
+        //button.classList.add("ButtonMensaje")
+
+
     } else {
-        error.classList.remove("error2")
-        if (!link) {
-            errorLink.classList.add("error2")
-        } else { errorLink.classList.remove("error2") }
+        errorLink.classList.remove("error2")
+        button.classList.remove("ButtonDesactivado")
+
     }
 
-
+    // Mandar el video al backend
+    const urlVideo = {url: info}
+    
 })
